d31157de5b91417aabcf4932d26ede6f23fca725
Ryu Tae Hyun <ryu200112@gmail.com>
2025-07-06 22:52:02 +0900

    🚨 npm run dev 수정

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col justify-center items-center mt-10">
-      home page
-    </div>
-  );
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  redirect("/home");
 }
